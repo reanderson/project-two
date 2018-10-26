0ea801d9ba9468c3063359c22698c756d4c24278
Tasima <re.anderson617@gmail.com>
2018-10-26 12:34:21 -0400

    I messed up locations of some of my test files, so I fixed that.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,24 +22,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public.entry.html"));
   });
 
-  /* //app.get("/", function(req, res) {
-    //res.sendFile({}).then(function(dbExamples) {
-      //res.render("index", {
-        msg: //"Welcome!",
-        //examples: dbExamples
-      //});
-    //});
-  //}); */
-
-  /* // Load example page and pass in an example by id
-  //app.get("/example/:id", function(req, res) {
-    //db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      //res.render("example", {
-        //example: dbExample
-      //});
-    //});
-  //}); */
-
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
